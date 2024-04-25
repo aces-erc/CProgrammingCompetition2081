91aa31e035b62be59b287d36e74de562289a48be
kaushalrijal <kaushalrijal091@gmail.com>
2024-04-25 16:51:13 +0545

    project added

---
+++ BCT/Veloce/loading.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <conio.h>
+#include <windows.h>
+
+void load()
+{
+    printf("\n\n\n:::     ::: :::::::::: :::         ::::::::   ::::::::  :::::::::: \n");
+    Sleep(75);
+    printf(":+:     :+: :+:        :+:        :+:    :+: :+:    :+: :+:        \n");
+    Sleep(75);
+    printf("+:+     +:+ +:+        +:+        +:+    +:+ +:+        +:+        \n");
+    Sleep(75);
+    printf("+#+     +:+ +#++:++#   +#+        +#+    +:+ +#+        +#++:++#   \n");
+    Sleep(75);
+    printf(" +#+   +#+  +#+        +#+        +#+    +#+ +#+        +#+        \n");
+    Sleep(75);
+    printf("  #+#+#+#   #+#        #+#        #+#    #+# #+#    #+# #+#        \n");
+    Sleep(75);
+    printf("    ###     ########## ##########  ########   ########  ##########\n");
+    Sleep(75);
+    printf("\n\n\n\n");
+    printf("Press any key to continue: ");
+    getch();
+}
